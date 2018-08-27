797ad21e42747daab1204c49acf18fc6a4d4c713
wuweijia <wuweijia@ccuol.com>
2018-08-27 16:44:54 +0800

    auth

---
+++ src/common/js/auth.js
@@ -0,0 +1,23 @@
+import 'babel-polyfill';
+import axios from 'axios';
+import { getQueryString } from './common';
+import config from './config';
+
+window.onload = async () => {
+  const userId = localStorage.getItem('userId');
+  if (!userId) {
+    const code = getQueryString('code');
+    if (!code) {
+      window.location.href = `https://open.weixin.qq.com/connect/oauth2/authorize?appid=${
+        config.appId
+      }&redirect_uri=${config.webHost}${
+        window.location.pathname
+      }&response_type=code&scope=snsapi_userinfo&state=park#wechat_redirect`;
+    } else {
+      const response = await axios.get(`${config.apiHost}/auth?code=${code}`);
+      alert(JSON.stringify(response.data));
+      localStorage.setItem('userId', response.data.userId);
+      // debugger;
+    }
+  }
+};
---
+++ src/common/js/config.js
@@ -0,0 +1,5 @@
+export default {
+  appId:'wx78ec611707faf6af',
+  webHost:'http://frp.j-coder.com',
+  apiHost:'http://nj.j-coder.com'
+};
