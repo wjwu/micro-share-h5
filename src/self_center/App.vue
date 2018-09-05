213b99711c63b8bb938db48aa206cb235824e8d6
wuweijia <wuweijia@ccuol.com>
2018-09-05 15:14:01 +0800

    self center

--- src/self_center/App.vue
+++ src/self_center/App.vue
@@ -1,3 +1,40 @@
 <template>
-  
+  <div class="main">
+    <div class="hd">
+      <div class="avatar"></div>
+    </div>
+    <div class="bd"></div>
+  </div>
 </template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss">
+.main {
+  .hd {
+    height: 11.5625rem;
+    background: url('./assets/images/timg.jpg') no-repeat;
+    background-size: cover;
+
+    &:after {
+      content: '';
+      width: 100%;
+      height: 11.5625rem;
+      position: absolute;
+      left: 0;
+      top: 0;
+      background: inherit;
+      background-size: cover;
+      filter: blur(2px);
+      z-index: 2;
+    }
+    .avatar {
+      
+    }
+  }
+}
+</style>
+
+
