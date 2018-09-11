f9b2c2d53d8150b6fd18e0b18bbae36bdbf48a9a
wuweijia <wuweijia@ccuol.com>
2018-09-11 13:30:20 +0800

    config

--- build/webpack.base.conf.babel.js
+++ build/webpack.base.conf.babel.js
@@ -8,15 +8,15 @@ import CopyWebpackPlugin from 'copy-webpack-plugin';
 
 const src = path.join(__dirname, '..', 'src');
 
-// const entryDirs = glob.sync('src/**/index.js').map(item => {
-//   const firstIdx = item.indexOf('/');
-//   const lastIdx = item.lastIndexOf('/');
-//   return item.substr(firstIdx + 1, lastIdx - firstIdx - 1);
-// });
+const entryDirs = glob.sync('src/**/index.js').map(item => {
+  const firstIdx = item.indexOf('/');
+  const lastIdx = item.lastIndexOf('/');
+  return item.substr(firstIdx + 1, lastIdx - firstIdx - 1);
+});
 
-const entryDirs = [
-  'bill/save'
-];
+// const entryDirs = [
+//   'bill/save'
+// ];
 
 const entry = {};
 const htmlPlugins = [];
