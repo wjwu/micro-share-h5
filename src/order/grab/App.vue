db3775840b73d7dff1dd68e6e7e7b9ff0322e107
wuweijia <wuweijia@ccuol.com>
2018-09-20 20:21:31 +0800

    grab

--- src/order/grab/App.vue
+++ src/order/grab/App.vue
@@ -1,39 +1,66 @@
 <template>
   <div v-if="showApp">
-    <div class="weui-cells__title">请设置订单抓取关键词(最多15个)</div>
-    <div class="weui-cells weui-cells_form">
-      <div v-if="keywords.length > 0">
-        <div class="weui-cell" v-for="(item,i) in keywords" :key="i">
-          <div class="weui-cell__bd">{{item}}</div>
-          <div class="weui-cell__fd">
-            <a href="javascript:;" class="weui-btn weui-btn_mini weui-btn_default" @click="handleEdit(i)">修改</a>
-            <a href="javascript:;" class="weui-btn weui-btn_mini weui-btn_warn" @click="handleRemove(i)">删除</a>
+    <div v-if="!selectedRoomId" class="group">
+      <div class="weui-cells__title">我的群列表</div>
+      <div class="weui-cells" v-for="item in myGroups" :key="item.id">
+        <a class="weui-cell weui-cell_access" href="javascript:;" @click="handleClick(item.id)">
+          <div class="weui-cell__bd">
+            <p>{{item.name}}</p>
           </div>
+          <div class="weui-cell__ft">设置抓取关键字</div>
+        </a>
+      </div>
+      <div class="weui-cells__title">商伴的群列表</div>
+      <div v-if="orderGroups.length > 0">
+        <div class="weui-cells" v-for="item in orderGroups" :key="item.id">
+          <a class="weui-cell weui-cell_access" href="javascript:;" @click="handleClick(item.id)">
+            <div class="weui-cell__bd">
+              <p>{{item.name}}</p>
+            </div>
+            <div class="weui-cell__ft">设置抓取关键字</div>
+          </a>
         </div>
       </div>
-      <div class="weui-loadmore weui-loadmore_line" v-else>
+      <div v-else class="weui-loadmore weui-loadmore_line">
         <span class="weui-loadmore__tips">暂无数据</span>
       </div>
     </div>
-    <div class="weui-cells__title">添加/修改</div>
-    <div class="weui-cells weui-cells_form">
-      <!-- <p>
+    <div v-else>
+      <div class="weui-cells__title">请设置订单抓取关键词(最多15个)</div>
+      <div class="weui-cells weui-cells_form">
+        <div v-if="keywords.length > 0">
+          <div class="weui-cell" v-for="(item,i) in keywords" :key="i">
+            <div class="weui-cell__bd">{{item}}</div>
+            <div class="weui-cell__fd">
+              <a href="javascript:;" class="weui-btn weui-btn_mini weui-btn_default" @click="handleEdit(i)">修改</a>
+              <a href="javascript:;" class="weui-btn weui-btn_mini weui-btn_warn" @click="handleRemove(i)">删除</a>
+            </div>
+          </div>
+        </div>
+        <div class="weui-loadmore weui-loadmore_line" v-else>
+          <span class="weui-loadmore__tips">暂无数据</span>
+        </div>
+      </div>
+      <div class="weui-cells__title">添加/修改</div>
+      <div class="weui-cells weui-cells_form">
+        <!-- <p>
           <span v-for="(item,i) in keywords" :key="i" class="weui-badge">{{item}}
             <i class="fa fa-remove" @click="handleRemove(i)"></i>
           </span>
         </p> -->
-      <div class="weui-cell">
-        <div class="weui-cell__hd">
-          <label class="weui-label">关键词</label>
-        </div>
-        <div class="weui-cell__bd">
-          <input v-model="keyword" class="weui-input" type="text" placeholder="请输入关键词" maxlength="20">
+        <div class="weui-cell">
+          <div class="weui-cell__hd">
+            <label class="weui-label">关键词</label>
+          </div>
+          <div class="weui-cell__bd">
+            <input v-model="keyword" class="weui-input" type="text" placeholder="请输入关键词" maxlength="20">
+          </div>
         </div>
       </div>
-    </div>
-    <div class="weui-btn-area">
-      <a class="weui-btn weui-btn_primary" href="javascript:;" @click="handleAdd">{{btnText}}</a>
-      <a class="weui-btn weui-btn_primary" href="javascript:;" @click="handleSave">保存</a>
+      <div class="weui-btn-area">
+        <a class="weui-btn weui-btn_primary" href="javascript:;" @click="handleAdd">{{btnText}}</a>
+        <a class="weui-btn weui-btn_primary" href="javascript:;" @click="handleSave">保存</a>
+      </div>
     </div>
   </div>
 </template>
@@ -48,9 +75,12 @@ export default {
   data() {
     return {
       showApp: false,
+      myGroups: [],
+      orderGroups: [],
       keywords: [],
       keyword: '',
       selectedIdx: null,
+      selectedRoomId: '',
       btnText: '添加'
     };
   },
@@ -58,15 +88,35 @@ export default {
     tryFunc(async () => {
       await auth();
       this.showApp = true;
-      const { data } = await axios.get(`${config.apiHost}/user`, {
+      const { data } = await axios.get(`${config.apiHost}/user/myAllRoom`, {
         headers: {
           userId: localStorage.getItem('userId')
         }
       });
-      this.keyWords = data;
+      for (let item of data) {
+        if (item.type === 'NORMAL') {
+          this.myGroups.push(item);
+        } else {
+          this.orderGroups.push(item);
+        }
+      }
     });
   },
   methods: {
+    handleClick(id) {
+      tryFunc(async () => {
+        const { data } = await axios.get(`${config.apiHost}/user/keyword`, {
+          params: {
+            roomId: id
+          },
+          headers: {
+            userId: localStorage.getItem('userId')
+          }
+        });
+        this.keywords = data.keyword.split(',');
+        this.selectedRoomId = id;
+      });
+    },
     handleAdd() {
       if (!this.keyword) {
         openToast('请输入关键词');
@@ -90,17 +140,40 @@ export default {
     },
     handleSave() {
       tryFunc(async () => {
-        await axios.post(
-          `${config.apiHost}`,
-          {},
+        await axios.put(
+          `${config.apiHost}/user/keyword`,
+          {
+            keyword: this.keywords.join(','),
+            roomId: this.selectedRoomId
+          },
           {
             headers: {
               userId: localStorage.getItem('userId')
             }
           }
         );
+        openToast('操作成功');
       });
     }
   }
 };
 </script>
+
+<style lang="scss">
+.group {
+  .weui-loadmore_line {
+    margin-top: 1.5rem;
+
+    .weui-loadmore__tips {
+      background-color: #f8f8f8 !important;
+    }
+  }
+}
+
+.weui-cell__fd {
+  .weui-btn + .weui-btn {
+    margin-top: 0;
+  }
+}
+</style>
+
--- src/robot/sendMsg/App.vue
+++ src/robot/sendMsg/App.vue
@@ -1,84 +1,86 @@
 <template>
-  <div class="main" v-if="showApp">
-     <div class="weui-cells__title">今日还剩 <b style="color:red;">{{times}}</b> 次群发消息</div>
-    <div class="weui-cells weui-cells_form">
-        <div class="weui-cell">
-            <div class="weui-cell__hd"><label class="weui-label">消息类型</label></div>
-            <div class="weui-cell__bd">
-                <select class="weui-select" v-model="selectType" name="select2">
-                    <option selected="selected" value="TEXT">文本</option>
-                    <option value="IMG">图片</option>
-                </select>
+    <div class="main" v-if="showApp">
+        <div class="weui-cells__title">今日还剩
+            <b style="color:red;">{{times}}</b> 次群发消息</div>
+        <div class="weui-cells weui-cells_form">
+            <div class="weui-cell">
+                <div class="weui-cell__hd">
+                    <label class="weui-label">消息类型</label>
+                </div>
+                <div class="weui-cell__bd">
+                    <select class="weui-select" v-model="selectType" name="select2">
+                        <option selected="selected" value="TEXT">文本</option>
+                        <option value="IMG">图片</option>
+                    </select>
+                </div>
             </div>
-        </div>
-        <div class="page__bd">
-            <div class="weui-gallery" id="gallery">
-                <span class="weui-gallery__img" id="galleryImg"></span>
-                <div class="weui-gallery__opr">
-                    <a href="javascript:" class="weui-gallery__del">
-                        <i class="weui-icon-delete weui-icon_gallery-delete"></i>
-                    </a>
+            <div class="page__bd">
+                <div class="weui-gallery" id="gallery">
+                    <span class="weui-gallery__img" id="galleryImg"></span>
+                    <div class="weui-gallery__opr">
+                        <a href="javascript:" class="weui-gallery__del">
+                            <i class="weui-icon-delete weui-icon_gallery-delete"></i>
+                        </a>
+                    </div>
                 </div>
             </div>
         </div>
-    </div>
-    <div class="weui-cells weui-cells_form" v-if="selectType === 'IMG'">
-        <div class="weui-cell">
-            <div class="weui-cell__bd">
-                <div class="weui-uploader">
-                    <div class="weui-uploader__hd">
-                        <p class="weui-uploader__title">图片</p>
-                        <div class="weui-uploader__info">0/1</div>
-                    </div>
-                    <div class="weui-uploader__bd">
-                        <ul class="weui-uploader__files" id="uploaderFiles">
-                            <!-- <li class="weui-uploader__file" style="background-image:url(./images/pic_160.png)"></li> -->
-                        </ul>
-                        <div class="weui-uploader__input-box">
-                            <input id="uploaderInput" class="weui-uploader__input" type="file" accept="image/*"
-                                multiple="">
+        <div class="weui-cells weui-cells_form" v-if="selectType === 'IMG'">
+            <div class="weui-cell">
+                <div class="weui-cell__bd">
+                    <div class="weui-uploader">
+                        <div class="weui-uploader__hd">
+                            <p class="weui-uploader__title">图片</p>
+                            <div class="weui-uploader__info">0/1</div>
+                        </div>
+                        <div class="weui-uploader__bd">
+                            <ul class="weui-uploader__files" id="uploaderFiles">
+                                <!-- <li class="weui-uploader__file" style="background-image:url(./images/pic_160.png)"></li> -->
+                            </ul>
+                            <div class="weui-uploader__input-box">
+                                <input id="uploaderInput" class="weui-uploader__input" type="file" accept="image/*" multiple="">
+                            </div>
                         </div>
                     </div>
                 </div>
             </div>
         </div>
-    </div>
-    <div class="weui-cells__title" v-if="selectType === 'TEXT'">消息内容</div>
-    <div class="weui-cells weui-cells_form" v-if="selectType === 'TEXT'">
-        <div class="weui-cell">
-            <div class="weui-cell__bd">
-                <textarea class="weui-textarea" placeholder="请输入群发消息内容" rows="3"></textarea>
-                <div class="weui-textarea-counter"><span>0</span>/200</div>
+        <div class="weui-cells__title" v-if="selectType === 'TEXT'">消息内容</div>
+        <div class="weui-cells weui-cells_form" v-if="selectType === 'TEXT'">
+            <div class="weui-cell">
+                <div class="weui-cell__bd">
+                    <textarea class="weui-textarea" placeholder="请输入群发消息内容" rows="3"></textarea>
+                    <div class="weui-textarea-counter">
+                        <span>0</span>/200</div>
+                </div>
             </div>
         </div>
-    </div>
-    <div class="weui-cells__title">请选择需要群发的群组和群</div>
-    <div class="weui-cells weui-cells_form">
-        <div class="weui-cells weui-cells_checkbox" v-for="room in rooms" :key="room.id">
-            <label class="weui-cell weui-check__label" for="s11">
-                <div class="weui-cell__hd">
-                    <input type="checkbox" class="weui-check" name="checkbox1" id="s11" checked="checked">
-                    <i class="weui-icon-checked"></i>
-                </div>
-                <div class="weui-cell__bd">
-                    <p>{{room.name}}</p>
-                </div>
-            </label>
+        <div class="weui-cells__title">请选择需要群发的群组和群</div>
+        <div class="weui-cells weui-cells_form">
+            <div class="weui-cells weui-cells_checkbox" v-for="room in rooms" :key="room.id">
+                <label class="weui-cell weui-check__label" for="s11">
+                    <div class="weui-cell__hd">
+                        <input type="checkbox" class="weui-check" name="checkbox1" id="s11" checked="checked">
+                        <i class="weui-icon-checked"></i>
+                    </div>
+                    <div class="weui-cell__bd">
+                        <p>{{room.name}}</p>
+                    </div>
+                </label>
+            </div>
+            <div class="weui-cell" v-if="!rooms || rooms.length === 0">暂无可用群</div>
         </div>
-        <div class="weui-cell" v-if="!rooms || rooms.length === 0">暂无可用群</div>
-    </div>
-    <div class="weui-btn-area">
-        <a class="weui-btn weui-btn_primary" :disabled="disabled" href="javascript:;">发送</a>
-    </div>
+        <div class="weui-btn-area">
+            <a class="weui-btn weui-btn_primary" :disabled="disabled" href="javascript:;">发送</a>
+        </div>
     </div>
 </template>
 
 <script>
-import axios from "axios";
-import { auth } from "../../common/js/auth";
-import config from "../../common/js/config";
-import { tryFunc } from "../../common/js/common";
-import weui from "weui.js";
+import axios from 'axios';
+import { auth } from '../../common/js/auth';
+import config from '../../common/js/config';
+import { tryFunc } from '../../common/js/common';
 
 export default {
   data() {
@@ -86,7 +88,7 @@ export default {
       showApp: false,
       user: {},
       times: null,
-      selectType: "TEXT",
+      selectType: 'TEXT',
       rooms: [],
       disabled: false
     };
@@ -97,17 +99,17 @@ export default {
       this.showApp = true;
       const { data } = await axios.get(`${config.apiHost}/user/myRoom`, {
         headers: {
-          userId: localStorage.getItem("userId")
+          userId: localStorage.getItem('userId')
         }
       });
       if (!data || data.length === 0) {
         this.disabled = true;
       }
       this.rooms = data;
-      this.times = localStorage.getItem("sendMsgTimes");
+      this.times = localStorage.getItem('sendMsgTimes');
       if (!this.times) {
         this.times = 5;
-        localStorage.setItem("sendMsgTimes", 5);
+        localStorage.setItem('sendMsgTimes', 5);
       }
     });
   },
