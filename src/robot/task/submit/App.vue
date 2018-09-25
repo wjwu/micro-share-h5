fd9310da1865bb7a3a653056b96673d07f0a595a
吴维佳 <wjwu@wuweijias-MacBook-Pro.local>
2018-09-25 20:50:31 +0800

    format

--- src/robot/task/submit/App.vue
+++ src/robot/task/submit/App.vue
@@ -337,35 +337,34 @@ export default {
           }
         );
         const dialog = weui.dialog({
-          content: "操作成功",
+          content: '操作成功',
           buttons: [
             {
-              label: "任务管理",
-              type: "default",
+              label: '任务管理',
+              type: 'default',
               onClick: () => {
-                window.location.href = "./list.html";
+                window.location.href = './list.html';
               }
             },
             {
-              label: "继续添加",
-              type: "primary",
+              label: '继续添加',
+              type: 'primary',
               onClick: () => {
                 this.rooms.forEach(item => (item.checked = false));
                 this.images = [];
-                this.messageType = "TEXT";
+                this.messageType = 'TEXT';
                 this.roomIds = [];
                 this.roomNames = [];
                 this.selectedDay = 0;
-                this.time = format(new Date(), "HH:mm");
-                this.sendType = "NOW";
-                this.content = "";
-                this.title = "";
+                this.time = format(new Date(), 'HH:mm');
+                this.sendType = 'NOW';
+                this.content = '';
+                this.title = '';
                 dialog.hide();
               }
             }
           ]
         });
-
       });
     }
   }
