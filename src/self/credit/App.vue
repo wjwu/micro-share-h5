41e95270e23629ff51f7769dacd36676aa57a1c6
wuweijia <wuweijia@ccuol.com>
2018-09-29 21:46:50 +0800

    credit

--- src/self/credit/App.vue
+++ src/self/credit/App.vue
@@ -1,18 +1,14 @@
 <template>
   <div v-if="showApp">
     <div class="weui-panel weui-panel_access">
-      <div class="weui-panel__hd">我的消息</div>
-      <div class="weui-panel__bd" v-if="msgs.length > 0 ">
-        <div class="weui-media-box weui-media-box_text" v-for="item in msgs" :key="item.id">
+      <div class="weui-panel__hd">我的信用记录</div>
+      <div class="weui-panel__bd" v-if="credits.length > 0 ">
+        <div class="weui-media-box weui-media-box_text" v-for="item in credits" :key="item.id">
           <a :href="item.link">
-            <h4 class="weui-media-box__title">{{item.title}}</h4>
-            <p class="weui-media-box__desc">{{item.text}}</p>
+            <h4 class="weui-media-box__title">{{Number(item.score)>0?`+${item.score}`:item.score}}</h4>
+            <p class="weui-media-box__desc">{{item.desc}}</p>
             <ul class="weui-media-box__info">
-              <li class="weui-media-box__info__meta">官方消息</li>
               <li class="weui-media-box__info__meta">{{item.createTime | time}}</li>
-              <!-- <li class="weui-media-box__info__meta weui-media-box__info__meta_extra">
-              <a :href="item.link">点击查看详情</a>
-            </li> -->
             </ul>
           </a>
         </div>
@@ -34,11 +30,11 @@
 <script>
 import axios from 'axios';
 import format from 'date-fns/format';
-import { auth } from '../common/js/auth';
-import config from '../common/js/config';
-import { tryFunc } from '../common/js/common';
-import Back from '../common/components/Back';
-import '../common/js/share';
+import { auth } from '../../common/js/auth';
+import config from '../../common/js/config';
+import { tryFunc } from '../../common/js/common';
+import Back from '../../common/components/Back';
+import '../../common/js/share';
 
 export default {
   components: {
@@ -47,19 +43,19 @@ export default {
   data() {
     return {
       showApp: false,
-      msgs: []
+      credits: []
     };
   },
   mounted() {
     tryFunc(async () => {
       await auth();
       this.showApp = true;
-      const { data } = await axios.get(`${config.apiHost}/user/myMsg`, {
+      const { data } = await axios.get(`${config.apiHost}/credit`, {
         headers: {
           userId: localStorage.getItem('userId')
         }
       });
-      this.msgs = data;
+      this.credits = data;
     });
   },
   filters: {
