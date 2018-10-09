fc93e68ae33e2eb460fece356c762b9c34375a7b
wuweijia <wuweijia@ccuol.com>
2018-10-09 13:55:24 +0800

    remove debugger

--- src/pay/App.vue
+++ src/pay/App.vue
@@ -44,7 +44,6 @@ export default {
           //   paySign: '70EA570631E4BB79628FBCA90534C63FF7FADD89' // 微信签名
           // },
           function(res) {
-            debugger;
             if (res.err_msg === 'get_brand_wcpay_request:ok') {
               // 使用以上方式判断前端返回,微信团队郑重提示：
               // res.err_msg将在用户支付成功后返回ok，但并不保证它绝对可靠。
