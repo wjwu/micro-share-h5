b7b1673f429be90a811f9df5a5b7f52d31d064f8
wuweijia <wuweijia@ccuol.com>
2018-10-11 11:17:46 +0800

    vip

--- src/vip/shopper/App.vue
+++ src/vip/shopper/App.vue
@@ -6,7 +6,7 @@
     <div class="page__hd top">
       <div class="top-info">
         <h1 class="page__title">我的管家</h1>
-        <span class="expire-time">有效期至：{{vipInfo.baseVipExpire | time}}</span>
+        <span class="expire-time" v-if="vipInfo.baseVipFlag">有效期至：{{vipInfo.baseVipExpire | time}}</span>
       </div>
       <p class="page__desc">建议您关注并置顶公众号，以方便您及时收取和处理相关进展</p>
     </div>
@@ -23,37 +23,37 @@
         </div>
         <p class="weui-grid__label">本周特价</p>
       </a>
-      <a href="../../item/shelves.html" class="weui-grid">
+      <a href="javascript:;" @click="handleJumpBase('/item/shelves.html')" class="weui-grid">
         <div class="weui-grid__icon">
           <img src="./assets/images/shop.png" alt="">
         </div>
         <p class="weui-grid__label">店铺货架</p>
       </a>
-      <a href="../../bill/save.html" class="weui-grid">
+      <a href="javascript:;" @click="handleJumpBase('/bill/save.html')" class="weui-grid">
         <div class="weui-grid__icon">
           <img src="./assets/images/account.png" alt="">
         </div>
         <p class="weui-grid__label">电子账簿</p>
       </a>
-      <a href="../../other/toker.html" class="weui-grid">
+      <a href="javascript:;" @click="handleJumpBase('/other/toker.html')" class="weui-grid">
         <div class="weui-grid__icon">
           <img src="./assets/images/qrcode.png" alt="">
         </div>
         <p class="weui-grid__label">引流拓客</p>
       </a>
-      <a href="../robot/grab.html" class="weui-grid">
+      <a href="javascript:;" @click="handleJumpBase('/robot/grab.html')" class="weui-grid">
         <div class="weui-grid__icon">
           <img src="./assets/images/robot.png" alt="">
         </div>
         <p class="weui-grid__label">订单抓取</p>
       </a>
-      <a href="../robot/follow.html" class="weui-grid">
+      <a href="javascript:;" @click="handleJumpBase('/robot/follow.html')" class="weui-grid">
         <div class="weui-grid__icon">
           <img src="./assets/images/follow.png" alt="">
         </div>
         <p class="weui-grid__label">关注人</p>
       </a>
-      <a href="/other/poster/template_hot.html" class="weui-grid">
+      <a href="javascript:;" @click="handleJumpBase('/other/poster/template_hot.html')" class="weui-grid">
         <div class="weui-grid__icon">
           <img src="./assets/images/banner.png" alt="">
         </div>
@@ -69,20 +69,19 @@
       </div>
     </div>
     <div class="weui-grids content">
-      <div class="mask" v-if="!vipInfo.advVipFlag" @click="handleClickMask"></div>
-      <a href="/robot/task/list.html" class="weui-grid">
+      <a href="javascript:;" @click="handleJumpShopper('/robot/task/list.html')" class="weui-grid">
         <div class="weui-grid__icon">
           <img src="./assets/images/msg.png" alt="">
         </div>
         <p class="weui-grid__label">一键群发</p>
       </a>
-      <a href="/partner/share.html" class="weui-grid">
+      <a href="javascript:;" @click="handleJumpShopper('/partner/share.html')" class="weui-grid">
         <div class="weui-grid__icon">
           <img src="./assets/images/hand.png" alt="">
         </div>
         <p class="weui-grid__label">商伴共享</p>
       </a>
-      <a href="/group/setup.html" class="weui-grid">
+      <a href="javascript:;" @click="handleJumpShopper('/group/setup.html')" class="weui-grid">
         <div class="weui-grid__icon">
           <img src="./assets/images/newuser.png" alt="">
         </div>
@@ -99,13 +98,13 @@
 </template>
 
 <script>
-import format from "date-fns/format";
-import weui from "weui.js";
-import { auth, checkIsMember } from "../../common/js/auth";
-import { tryFunc } from "../../common/js/common";
-import Bar from "../../common/components/Bar";
-import defaultHeadPhone from "./assets/images/user.png";
-import "../../common/js/share";
+import format from 'date-fns/format';
+import weui from 'weui.js';
+import { auth, checkIsMember } from '../../common/js/auth';
+import { tryFunc } from '../../common/js/common';
+import Bar from '../../common/components/Bar';
+import defaultHeadPhone from './assets/images/user.png';
+import '../../common/js/share';
 
 export default {
   components: {
@@ -114,30 +113,40 @@ export default {
   data() {
     return {
       showApp: false,
-      headPhoto: localStorage.getItem("headPhoto") || defaultHeadPhone,
-      vipInfo: null
+      headPhoto: localStorage.getItem('headPhoto') || defaultHeadPhone,
+      vipInfo: {}
     };
   },
   mounted() {
     tryFunc(async () => {
       await auth();
-      this.vipInfo = await checkIsMember();
       this.showApp = true;
+      this.vipInfo = await checkIsMember();
     });
   },
   methods: {
-    handleClickMask() {
-      weui.confirm(
-        "购买店长版VIP即可使用管家工具，是否前往购买页面购买？",
-        () => {
-          window.location.href = "/pay.html";
-        }
-      );
+    handleJumpShopper(url) {
+      if (!this.vipInfo.advVipFlag) {
+        weui.confirm('您尚未购买店长版VIP功能，是否前往购买页面购买？', () => {
+          window.location.href = '/pay.html';
+        });
+      } else {
+        window.location.href = url;
+      }
+    },
+    handleJumpBase(url) {
+      if (!this.vipInfo.baseVipFlag) {
+        weui.confirm('您尚未购买基础版VIP功能，是否前往购买页面购买？', () => {
+          window.location.href = '/pay.html';
+        });
+      } else {
+        window.location.href = url;
+      }
     }
   },
   filters: {
     time: val => {
-      return format(val, "YYYY-MM-DD HH:mm:ss");
+      return format(val, 'YYYY-MM-DD HH:mm:ss');
     }
   }
 };
@@ -170,16 +179,6 @@ export default {
 .content {
   position: relative;
   background-color: white;
-}
-
-.mask {
-  position: absolute;
-  left: 0;
-  top: 0;
-  bottom: 0;
-  right: 0;
-  background-color: rgba(0, 0, 0, 0.4);
-  z-index: 99;
 }
 
 .expire-time {
