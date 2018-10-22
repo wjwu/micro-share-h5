64cc124de5633f7c0f7c7c624163db8b467a664a
wenjie <1569925@qq.com>
2018-10-22 11:37:23 +0800

    change qrcode

--- src/qrcode/App.vue
+++ src/qrcode/App.vue
@@ -2,7 +2,7 @@
   <div>
     <div class="weui-gallery" style="display: block;">
         <span class="weui-gallery__img" style="width: 80%;margin: 4rem auto;">
-            <img src="http://static.fangzhoubuluo.com/robot.jpg" style="width:100%">
+            <img src="http://static.fangzhoubuluo.com/qrcode/robot.jpg" style="width:100%">
         </span>
         <div class="weui-gallery__opr" style="line-height:1.5;padding-bottom: 4rem;font-weight:900;">没发现您有自己的微信群，请长按识别二维码添加机器人。并将机器人拉入您的群中。
         </div>
