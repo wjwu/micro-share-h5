fce5009725c7f20a20c736c3acf6b1ebf1f1ed78
wenjie <1569925@qq.com>
2018-10-22 19:42:58 +0800

    change qrcode

--- src/qrcode/App.vue
+++ src/qrcode/App.vue
@@ -1,19 +1,18 @@
 <template>
-  <div>
-    <div class="weui-gallery" style="display: block;">
-        <span class="weui-gallery__img" style="width: 80%;margin: 4rem auto;">
-            <img src="http://static.fangzhoubuluo.com/qrcode/review.jpg" style="width:100%">
-        </span>
-        <div class="weui-gallery__opr" style="line-height:1.5;padding-bottom: 4rem;font-weight:900;">没发现您有自己的微信群，请长按识别二维码添加机器人。并将机器人拉入您的群中。
-        </div>
-    </div>
-  </div>
+  <result type="fail" title="没有微信群" sub-title="没发现您有自己的微信群，请长按识别二维码添加审核机器人。并将机器人拉入您的群中。">
+    <p><img src="http://static.fangzhoubuluo.com/qrcode/review.jpg" style="width:15rem;height:15rem;" alt=""></p>
+    <template slot="buttons">
+      <a href="/self/center.html" class="weui-btn weui-btn_default">返回个人中心</a>
+    </template>
+  </result>
 </template>
 
 <script>
-import '../common/js/share';
+import Result from '../common/components/Result';
 
 export default {
+  components: {
+    Result
+  }
 };
 </script>
-
