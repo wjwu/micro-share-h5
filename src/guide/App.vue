36563ddec3efd2392c16c01d462089a354b62dcc
wenjie <1569925@qq.com>
2018-10-24 11:32:51 +0800

    add page

---
+++ src/guide/App.vue
@@ -0,0 +1,39 @@
+<template>
+  <div>
+    前言：<br />
+    在使用管家中每个关键环节都有提示或说明指引您的使用，在使用过程中如有疑问，人工客服也会协助您完成各功能模块的使用。
+    完成管家购买后，相应的功能将会自动开通，您需要扫描添加机器人，并拉入您需要管理的社群即可开始使用。<br />
+    <center><img src="http://static.fangzhoubuluo.com/matchHelp/14.png" /></center><br />
+    <b>店长模块：</b>以本周特价模块功能展示为例<br />
+    第一步：点击“引流拓客”，进入店铺设置界面<br />
+    <center><img src="http://static.fangzhoubuluo.com/matchHelp/15.png" /></center><br />
+    第二步：设置店铺信息：（只首次使用需要设置）<br />
+    <center><img src="http://static.fangzhoubuluo.com/matchHelp/16.png" /></center><br />
+    第三步商品上传：（以本周特价为例），素材可从相册导入图片、亦可采用相机时时拍摄图片<br />
+    <center><img src="http://static.fangzhoubuluo.com/matchHelp/17.png" /></center><br />
+    第四步对外分享：商品对外在微信群中分享展现形式（可直接分享到朋友圈、好友、各种微信群，亦可分享给管家机器人实现多商伴微信群一键送达）<br />
+    <center><img src="http://static.fangzhoubuluo.com/matchHelp/18.png" /></center><br />
+    商品展现形式：客户在微信群中进入您的店铺链接进入商品展现形式（含商品信息、店铺水印、店铺地址、您的二维码等信息）<br />
+    <center><img src="http://static.fangzhoubuluo.com/matchHelp/19.png" /></center><br />
+    客户在微信群中进入您的店铺链接进入商品展现形式（客户可扫描您二维码实现引流、实体店可上门消费）<br />
+    <center><img src="http://static.fangzhoubuluo.com/matchHelp/20.png" /></center><br />
+    客户点击“联系商家购买”输入电话号码后，订单信息通过公众号信息立即上报给您,你可及时和买家联系<br />
+    <center><img src="http://static.fangzhoubuluo.com/matchHelp/21.png" /></center><br />
+    订单抓取：根据您设定商品关键词，在您和你商伴以及您朋友微信群里，有客户咨询包含您设定的商品关键词信息，管家将实时通过公众号信息上报给您，让您眼睛不用盯着微信群翻滚信息流也能不错过潜在订单：<br />
+    <center><img src="http://static.fangzhoubuluo.com/matchHelp/22.png" /></center><br />
+    关注人：根据您设定关注人，在您和你商伴以及您朋友微信群里，有关注人发言，管家将实时通过公众号信息上报给您，让您及时知晓关注人动态并回复：<br />
+    <center><img src="http://static.fangzhoubuluo.com/matchHelp/23.png" /></center><br />
+    一键群发：您可以通过“一键群发”在您自己群、朋友和商伴群中发送您的商品信息：可定时、即时发送文本、图片（含二维码）、分享卡片链接等信息<br />
+    <center><img src="http://static.fangzhoubuluo.com/matchHelp/24.png" /></center><br />
+    商伴共享：您和朋友之间可以通过“商伴共享”分享功能直接结成商伴，共享社群和管家资源，体验管家各种功能；参与商伴匹配的商伴自动直接进入您的商伴群列表，您亦可在商伴的社群中发送包含您店铺信息的商品海报<br />
+    <center><img src="http://static.fangzhoubuluo.com/matchHelp/25.png" /></center><br />
+    还有其他的管家功能您可以逐步体会，人工客服会协助您实现良好体验。
+  </div>
+</template>
+
+<script>
+import "../common/js/share";
+
+export default {};
+</script>
+
--- src/match/App.vue
+++ src/match/App.vue
@@ -10,13 +10,13 @@
     <center><img src="http://static.fangzhoubuluo.com/matchHelp/1.png" /></center><br />
     <center><img src="http://static.fangzhoubuluo.com/matchHelp/2.png" /></center><br />
     第二步：扫描二维码添加好友（按提示）<br />
-    <center><img src="http://static.fangzhoubuluo.com/matchHelp/11.png" /></center>
+    <center><img src="http://static.fangzhoubuluo.com/matchHelp/12.png" /></center>
     <br />
     第三步：拉入好友入参与匹配的群（按提示）<br /><br />
     第四步：填写群资料并提交（读提示与注意事项）<br /><br />
     后续的您主要只需要等待匹配进展通知即可,系统会自动给您匹配商伴（在匹配环节中，您可以到“我的商伴”中查看进展，也可手动匹配选择您的商伴）<br />
     <center><img src="http://static.fangzhoubuluo.com/matchHelp/4.png" /></center><br />
-    <center><img src="http://static.fangzhoubuluo.com/matchHelp/5.png" /></center><br />
+    <center><img src="http://static.fangzhoubuluo.com/matchHelp/13.png" /></center><br />
     <br />
     温馨特别提示：<br />
     1、匹配每个环节进展信息都会及时在您公众号窗口提示，您可在“我的消息”菜单中查看。建议您将公众号置顶，以及时收取和处理相关进展。<br />
