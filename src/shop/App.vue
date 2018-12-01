40058b0b1445d8d0010f62cc0ea8a4b5e71a428a
wuweijia <wuweijia@ccuol.com>
2018-12-01 17:39:30 +0800

    shop

---
+++ src/shop/App.vue
@@ -0,0 +1,249 @@
+<template>
+  <div v-if="showApp && shop">
+    <div class="main">
+      <div class="hd">
+        <div class="avatar"></div>
+        <div class="info">
+          <div class="name">{{shop.address}}</div>
+        </div>
+      </div>
+      <div class="notice">
+        <p>店长公告：</p>
+        <marquee>{{shop.notice}}</marquee>
+      </div>
+      <div class="activity">
+        <h2>优惠活动<span>/ FLASH SALE</span></h2>
+      </div>
+      <div class="special">
+        <h2>本周特价<span>/ FLASH SALE</span></h2>
+        <ul>
+          <li>
+            <a href="https://static2.flowerplus.cn/p/R39va8eu50011.html">
+              <div class="pic">
+                <img src="https://oss.flowerplus.cn/homepage/15326010469403.jpg">
+                <div class="prompt">平台新用户送花瓶</div>
+              </div>
+              <h3>一千零一夜同款简花</h3>
+              <p class="desc">经典单品，新人首选。</p>
+              <p class="price"><span class="new">¥99元/4束</span>
+              </p>
+            </a>
+          </li>
+        </ul>
+      </div>
+      <div class="newer">
+        <h2>店长推荐<span>/ FLASH SALE</span></h2>
+      </div>
+      <div class="all">
+        <h2>所有商品<span>/ FLASH SALE</span></h2>
+      </div>
+    </div>
+    <cust-bar></cust-bar>
+  </div>
+</template>
+<script>
+import axios from '../common/js/axios';
+import { auth } from '../common/js/auth';
+import { tryFunc, getQueryString } from '../common/js/common';
+import CustBar from '../common/components/CustBar';
+export default {
+  components: {
+    CustBar
+  },
+  data() {
+    return {
+      showApp: false,
+      shop: null,
+      userId: getQueryString('userId')
+    };
+  },
+  mounted() {
+    tryFunc(async () => {
+      await auth();
+      this.showApp = true;
+      const { data } = await axios.get('/user/shopInfoById', {
+        params: {
+          userId: this.userId
+        }
+      });
+      this.shop = data;
+      window.document.title = this.shop.name;
+      this.$nextTick(() => {
+        this.$el.querySelector('.hd').style.background = `url("${
+          this.shop.background
+        }") no-repeat`;
+        this.$el.querySelector('.avatar').style.background = `url("${
+          this.shop.logo
+        }") no-repeat`;
+      });
+    });
+  }
+};
+</script>
+
+<style lang="scss">
+body {
+  background-color: #fff;
+  font-family: 'Helvetica Neue', Helvetica, 'PingFang SC', 'Hiragino Sans GB',
+    'Microsoft YaHei', '微软雅黑', Arial, sans-serif;
+}
+.hd {
+  position: relative;
+  height: 11.5625rem;
+  background-size: cover !important;
+  &:after {
+    content: '';
+    width: 100%;
+    height: 11.5625rem;
+    position: absolute;
+    left: 0;
+    top: 0;
+    background: inherit;
+    background-size: inherit;
+    filter: blur(3px);
+    z-index: 2;
+  }
+  .avatar {
+    position: absolute;
+    top: 1.5625rem;
+    left: calc(50% - 2.625rem);
+    width: 5.25rem;
+    height: 5.25rem;
+    border-radius: 50%;
+    border: 1px solid #fff;
+    background-size: cover !important;
+    z-index: 3;
+  }
+  .info {
+    position: absolute;
+    width: 100%;
+    top: 7.5rem;
+    z-index: 3;
+    .name {
+      font-size: 1.125rem;
+      color: #fff;
+      text-align: center;
+    }
+  }
+}
+
+.notice {
+  margin-top: 0.5rem;
+  padding: 0.5rem 1.25rem;
+  display: flex;
+  p {
+    flex-shrink: 0;
+    font-size: 1rem;
+  }
+  marquee {
+    font-size: 0.875rem;
+    color: #333;
+  }
+  background-color: #f4f4f4;
+}
+
+.activity,
+.special,
+.newer,
+.all {
+  h2 {
+    padding: 3.125rem 1.25rem 1.25rem;
+    color: #333;
+    font-size: 1.125rem;
+    letter-spacing: 1;
+    line-height: 1.5625rem;
+    font-weight: 700;
+
+    span {
+      font-weight: 0.875rem;
+      font-weight: lighter;
+      color: #b2b2b2;
+      letter-spacing: 1;
+      line-height: 1.5625rem;
+      padding-left: 0.375rem;
+    }
+  }
+
+  ul {
+    display: flex;
+    flex-wrap: wrap;
+    justify-content: space-between;
+    padding: 0 1.25rem;
+
+    li {
+      position: relative;
+      flex: 0 0 48%;
+      margin-bottom: 1rem;
+      overflow: hidden;
+
+      a {
+        display: block;
+        background: transparent;
+        text-decoration: none;
+        -webkit-tap-highlight-color: transparent;
+        color: #453b38;
+      }
+
+      img {
+        width: 100%;
+        height: 10rem;
+      }
+
+      .badge {
+        position: absolute;
+        top: 0em;
+        right: 0rem;
+        font-size: 1rem;
+        border-radius: 0px;
+      }
+
+      h3 {
+        color: #333;
+        font-size: 0.875rem;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+        line-height: 1;
+        letter-spacing: 1px;
+        padding-top: 0.625rem;
+        font-weight: bold;
+      }
+
+      .desc {
+        padding: 0.5rem 0 0.875rem;
+        font-size: 0.75rem;
+        line-height: 1;
+        color: #666;
+        letter-spacing: 1px;
+        overflow: hidden;
+        text-overflow: ellipsis;
+        white-space: nowrap;
+        min-height: 2.125rem;
+      }
+
+      .price {
+        color: #333;
+        font-size: 0.75rem;
+        font-weight: 700;
+        line-height: 1;
+        display: flex;
+        align-items: flex-end;
+        justify-content: space-between;
+
+        small {
+          color: #b2b2b2;
+          font-size: 0.625rem;
+          font-weight: 400;
+        }
+      }
+    }
+  }
+}
+.activity {
+  h2 {
+    padding: 0.5rem 1.25rem 1.25rem;
+  }
+}
+</style>
+
+
