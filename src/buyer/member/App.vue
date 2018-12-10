76a7240951ba6e2c5bb9bf609d5d294be4052d27
wenjie <1569925@qq.com>
2018-12-10 16:41:22 +0800

    change style

--- src/buyer/member/App.vue
+++ src/buyer/member/App.vue
@@ -1,34 +1,18 @@
 <template>
-  <div class="weui-panel">
+  <div class="weui-panel" v-if="showApp">
     <div class="weui-panel__hd">
       <label>会员昵称</label>
       <label>券数量</label>
       <label>积分数</label>
       <label>核销积分</label>
     </div>
-    <div class="weui-panel__bd">
-      <div class="weui-media-box weui-media-box_text">
+    <div class="weui-panel__bd" v-if="memberList.length>0">
+      <div v-for="(item,i) in memberList" :key="i"  class="weui-media-box weui-media-box_text">
         <p class="weui-media-box__desc">
-          <label>会员昵称</label>
-          <label>券数量</label>
-          <label>积分数</label>
-          <label>核销积分</label>
-        </p>
-      </div>
-      <div class="weui-media-box weui-media-box_text">
-        <p class="weui-media-box__desc">
-          <label>会员昵称</label>
-          <label>券数量</label>
-          <label>积分数</label>
-          <label>核销积分</label>
-        </p>
-      </div>
-      <div class="weui-media-box weui-media-box_text">
-        <p class="weui-media-box__desc">
-          <label>会员昵称</label>
-          <label>券数量</label>
-          <label>积分数</label>
-          <label>核销积分</label>
+          <label>{{item.name}}</label>
+          <label>{{item.coupons}}</label>
+          <label>{{item.score}}</label>
+          <label @click="handleClick(item.memberId)">核销积分</label>
         </p>
       </div>
     </div>
@@ -36,7 +20,33 @@
 </template>
 
 <script>
-export default {};
+
+import { tryFunc } from "../../common/js/common";
+import axios from "../../common/js/axios";
+
+export default {
+
+data() {
+    return {
+      showApp: false,
+      memberList: [],
+    };
+  },
+  mounted() {
+    tryFunc(async () => {
+      this.showApp = true;
+      const { data } = await axios.get("/shop/memberList");
+      this.memberList = data;
+    });
+  },methods: {
+    handleClick(memberId){
+      //TODO 弹窗输入数值，值不能大于当前的item.score
+      const num = 5;
+      axios.put(`/shop/${memberId}?score=${num}`);
+      window.location.reload();
+    }
+  }
+};
 </script>
 
 <style lang="scss">
--- src/buyer/stats/App.vue
+++ src/buyer/stats/App.vue
@@ -1,26 +1,58 @@
 <template>
-  <div>
+  <div v-if="showApp">
     <weui-cells>
-      <weui-cell label="赠送券总数："></weui-cell>
-      <weui-cell label="已核销券总数："></weui-cell>
-      <weui-cell label="剩余券总数："></weui-cell>
+      <weui-cell label="赠送券总数：">{{data.sendedNums}}</weui-cell>
+      <weui-cell label="已核销券总数：">{{data.usedNums}}</weui-cell>
+      <weui-cell label="剩余券总数：">{{data.sendedNums - data.usedNums}}</weui-cell>
     </weui-cells>
     <weui-cells>
-      <weui-cell label="会员总积分："></weui-cell>
-      <weui-cell label="已核销总积分："></weui-cell>
-      <weui-cell label="剩余积分："></weui-cell>
+      <weui-cell label="会员总积分：">{{data.totalScore}}</weui-cell>
+      <weui-cell label="已核销总积分：">{{data.totalUsedScore}}</weui-cell>
+      <weui-cell label="剩余积分：">{{data.totalScore - data.totalUsedScore}}</weui-cell>
     </weui-cells>
     <weui-cells>
-      <weui-cell label="会员数量："></weui-cell>
-      <weui-cell-access label="会员名单" empty-body empty-foot></weui-cell-access>
+      <weui-cell label="会员数量：">{{data.totalMember}}</weui-cell>
+      <a class="weui-cell weui-cell_access" href="/buyer/member.html">
+        <div class="weui-cell__hd">
+          <img
+            src="./assets/images/vip.png"
+            style="width:20px;margin-right:5px;display:block"
+          >
+        </div>
+        <div class="weui-cell__bd">
+          <p>会员名单</p>
+        </div>
+        <div class="weui-cell__ft"></div>
+      </a>
     </weui-cells>
   </div>
 </template>
 
 <script>
-import { WeuiCells, WeuiCell, WeuiCellAccess } from '../../common/components';
+import { WeuiCells, WeuiCell, WeuiCellAccess } from "../../common/components";
+import { tryFunc } from "../../common/js/common";
+import axios from "../../common/js/axios";
 
 export default {
+  data() {
+    return {
+      showApp: false,
+      data: {
+        sendedNums: 0,
+        usedNums: 0,
+        totalScore: 0,
+        totalUsedScore: 0,
+        totalMember: 0
+      }
+    };
+  },
+  mounted() {
+    tryFunc(async () => {
+      this.showApp = true;
+      const { data } = await axios.get("/shop/data");
+      this.data = data;
+    });
+  },
   components: {
     WeuiCells,
     WeuiCell,
@@ -30,7 +62,7 @@ export default {
 </script>
 
 <style lang="scss">
-.weui-label{
+.weui-label {
   width: 120px !important;
 }
 </style>
