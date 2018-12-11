b288ad6b4ac376db6019a1bf089e3d971aee4139
wenjie <1569925@qq.com>
2018-12-11 11:19:13 +0800

    change bug

--- src/buyer/settlement/App.vue
+++ src/buyer/settlement/App.vue
@@ -2,21 +2,33 @@
   <div>
     <weui-cells-title>请选择省市区：</weui-cells-title>
     <weui-cells>
+      <weui-cell label="姓名">
+        <input v-model="name" class="weui-input" type="text" placeholder="请输入您的姓名">
+      </weui-cell>
+      <weui-cell label="手机号">
+        <input
+          v-model="phone"
+          class="weui-input"
+          type="number"
+          pattern="[0-9]*"
+          placeholder="请输入手机号"
+        >
+      </weui-cell>
       <weui-cell-select label="省份">
         <select class="weui-select" v-model="selectedProvince" @change="handleProvinceChange">
-          <option value="">请选择省份</option>
+          <option value>请选择省份</option>
           <option :value="item.code" v-for="item in provinces" :key="item.code">{{item.name}}</option>
         </select>
       </weui-cell-select>
       <weui-cell-select label="城市">
         <select class="weui-select" v-model="selectedCity" @change="handleCityChange">
-          <option value="">请选择城市</option>
+          <option value>请选择城市</option>
           <option :value="item.code" v-for="item in cities" :key="item.code">{{item.name}}</option>
         </select>
       </weui-cell-select>
       <weui-cell-select label="区县">
         <select class="weui-select" v-model="selectedCounty">
-          <option value="">请选择区县</option>
+          <option value>请选择区县</option>
           <option :value="item.code" v-for="item in counties" :key="item.code">{{item.name}}</option>
         </select>
       </weui-cell-select>
@@ -29,7 +41,12 @@
     </weui-cells>
     <weui-panel>
       <div v-if="products.length > 0">
-        <a v-for="product in products" :key="product.id" :href="`/product.html?productId=${product.id}`" class="weui-media-box weui-media-box_appmsg">
+        <a
+          v-for="product in products"
+          :key="product.id"
+          :href="`/product.html?productId=${product.id}`"
+          class="weui-media-box weui-media-box_appmsg"
+        >
           <div class="weui-media-box__hd">
             <img class="weui-media-box__thumb" :src="product.imgUrl">
           </div>
@@ -74,14 +91,11 @@
     <weui-cells>
       <weui-cell-select label="优惠券">
         <select class="weui-select" :disabled="coupons.length === 0" v-model="selectCoupon">
-          <option value="" v-if="coupons.length === 0">暂无可以用优惠券</option>
-          <option value="" v-else>请选择优惠券</option>
+          <option value v-if="coupons.length === 0">暂无可以用优惠券</option>
+          <option value v-else>请选择优惠券</option>
           <option :value="item.id" v-for="item in coupons" :key="item.id">{{item.price}}元</option>
         </select>
       </weui-cell-select>
-      <weui-cell label="手机号">
-        <input v-model="phone" class="weui-input" type="number" pattern="[0-9]*" placeholder="请输入手机号">
-      </weui-cell>
     </weui-cells>
     <weui-btn-area>
       <weui-btn type="primary" @click="handleSubmit" :disabled="products.length === 0">确认下单</weui-btn>
@@ -90,8 +104,8 @@
 </template>
 
 <script>
-import axios from '../../common/js/axios';
-import { auth } from '../../common/js/auth';
+import axios from "../../common/js/axios";
+import { auth } from "../../common/js/auth";
 import {
   WeuiPanel,
   WeuiLoadMoreLine,
@@ -102,17 +116,17 @@ import {
   WeuiCell,
   WeuiTextarea,
   WeuiCellSelect
-} from '../../common/components';
+} from "../../common/components";
 import {
   tryFunc,
   getQueryString,
   openAlert,
   checkPhone
-} from '../../common/js/common';
-import regions from '../../common/js/regions';
-import '../../common/js/share.js';
+} from "../../common/js/common";
+import regions from "../../common/js/regions";
+import "../../common/js/share.js";
 
-const visitShopUserId = localStorage.getItem('visitShopUserId');
+const visitShopUserId = localStorage.getItem("visitShopUserId");
 const cartKey = `cart_${visitShopUserId}`;
 let cart = [];
 const strCart = localStorage.getItem(cartKey);
@@ -152,30 +166,39 @@ export default {
   data() {
     return {
       showApp: false,
-      productIds: getQueryString('productIds'),
+      productIds: getQueryString("productIds"),
       products: [],
       coupons: [],
-      selectCoupon: '',
-      address: '',
-      selectedProvince: '',
-      selectedCity: '',
-      selectedCounty: '',
+      selectCoupon: "",
+      address: "",
+      selectedProvince: "",
+      selectedCity: "",
+      selectedCounty: "",
       provinces: [],
       cities: [],
       counties: [],
-      phone: ''
+      phone: "",
+      name: ""
     };
   },
   created() {
-    const address = localStorage.getItem('address');
+    const address = localStorage.getItem("address");
     if (address) {
       this.address = address;
     }
-    const countyCode = localStorage.getItem('countyCode');
+    const phone = localStorage.getItem("phone");
+    if (phone) {
+      this.phone = phone;
+    }
+    const name = localStorage.getItem("name");
+    if (name) {
+      this.name = name;
+    }
+    const countyCode = localStorage.getItem("countyCode");
     if (countyCode) {
       this.setSelected(countyCode);
     } else {
-      this.provinces = regions.filter(item => item.code.endsWith('0000'));
+      this.provinces = regions.filter(item => item.code.endsWith("0000"));
     }
   },
   mounted() {
@@ -191,7 +214,7 @@ export default {
         }
       }
       this.products = response.data;
-      response = await axios.get('/user/coupon');
+      response = await axios.get("/user/coupon");
       if (response.data) {
         // 排序过滤优惠券
         this.coupons = response.data
@@ -207,18 +230,18 @@ export default {
       this.counties = regions.filter(
         item =>
           item.code.startsWith(countyCode.substr(0, 4)) &&
-          !item.code.endsWith('00')
+          !item.code.endsWith("00")
       );
       this.selectedCounty = countyCode;
 
       this.cities = regions.filter(
         item =>
           item.code.startsWith(countyCode.substr(0, 2)) &&
-          item.code.endsWith('00') &&
-          !item.code.endsWith('0000')
+          item.code.endsWith("00") &&
+          !item.code.endsWith("0000")
       );
 
-      this.provinces = regions.filter(item => item.code.endsWith('0000'));
+      this.provinces = regions.filter(item => item.code.endsWith("0000"));
       const city = this.cities.filter(
         item => item.code.substr(0, 4) === countyCode.substr(0, 4)
       )[0];
@@ -233,7 +256,7 @@ export default {
       this.cities = regions.filter(
         item =>
           item.code.startsWith(proviceCode.substr(0, 2)) &&
-          item.code.endsWith('00') &&
+          item.code.endsWith("00") &&
           item.code !== proviceCode
       );
       this.selectedCity = this.cities[0].code;
@@ -249,23 +272,27 @@ export default {
     },
     handleSubmit() {
       if (!this.selectedProvince) {
-        openAlert('请选择省份');
+        openAlert("请选择省份");
         return;
       }
       if (!this.selectedCity) {
-        openAlert('请选择城市');
+        openAlert("请选择城市");
         return;
       }
       if (!this.selectedCounty) {
-        openAlert('请选择区县');
+        openAlert("请选择区县");
         return;
       }
       if (!this.address) {
-        openAlert('请输入详细地址');
+        openAlert("请输入详细地址");
         return;
       }
       if (!checkPhone(this.phone)) {
-        openAlert('手机号码格式不正确');
+        openAlert("手机号码格式不正确");
+        return;
+      }
+      if (!this.name) {
+        openAlert("请输入姓名");
         return;
       }
       const proviceName = regions.filter(
@@ -278,14 +305,17 @@ export default {
         item => item.code === this.selectedCounty
       )[0].name;
       tryFunc(async () => {
-        localStorage.setItem('address', this.address);
-        localStorage.setItem('countyCode', this.selectedCounty);
-        await axios.post('/buyer/order/', {
+        localStorage.setItem("address", this.address);
+        localStorage.setItem("countyCode", this.selectedCounty);
+        localStorage.setItem("phone", this.phone);
+        localStorage.setItem("name", this.name);
+        await axios.post("/buyer/order/", {
           price: this.total - this.couponPrice,
           coupon: this.couponPrice,
           couponId: this.selectCoupon,
           address: proviceName + cityName + countyName + this.address,
           phone: this.phone,
+          name: this.name,
           orderItemDtoList: this.products.map(item => {
             return {
               itemId: item.id,
@@ -293,7 +323,7 @@ export default {
             };
           })
         });
-        const productIds = this.productIds.split(',');
+        const productIds = this.productIds.split(",");
         let tmp = [];
         for (let item of cart) {
           if (!productIds.includes(item.productId.toString())) {
@@ -302,7 +332,7 @@ export default {
         }
         localStorage.setItem(cartKey, JSON.stringify(tmp));
         setTimeout(() => {
-          window.location.href = './success.html';
+          window.location.href = "./success.html";
         });
       });
     }
